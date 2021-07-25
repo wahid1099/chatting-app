adac25da92748990807e33abb1e8f6a82fc33c55
Abdul wahid <40333979+wahid1099@users.noreply.github.com>
2021-07-25 17:02:29 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // creating express server
 
-
 const express = require("express");
 const app=express();
 const http = require("http");
@@ -10,6 +9,7 @@ const expressserver = http.createServer(app);
 // creating socket server
 const {Server}=require('socket.io');
 const io=new Server(expressserver);
+
 // server routing
 app.get('/',function (req,res){
     res.sendFile(__dirname+"/index.html");
@@ -17,13 +17,30 @@ app.get('/',function (req,res){
 })
 
 // sokcet connecction
+const users = {};
 
 io.on('connection',function (socket){
     console.log("new user conected");
     socket.on('chat',function (msg){
-   io.emit('chat_transfer',msg);
+
+        socket.broadcast.emit('receive', {message: msg, name: users[socket.id]})
+   // io.emit('chat_transfer',msg);
+
+
 
     })
+    socket.on('new-user-joined', name =>{
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+
+    });
+
+
+    // If someone leaves the chat, let others know
+    socket.on('disconnect', message =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
 
 })
 // port listenig
